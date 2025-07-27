8ac3e07f90fb0bf924f819abebf15c641110a1d5
jasmeet singh <asujasu71@gmail.com>
2025-07-27 13:13:22 +0530

    changes

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 async function main() {
-  await mongoose.connect("mongodb+srv://jasmeetdb:Jasmeet%401234@bookingsite.l1vxeep.mongodb.net/user");
+  await mongoose.connect("process.env.DB_connect_key");
 
 
   
